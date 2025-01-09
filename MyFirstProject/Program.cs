0074b4ac4dac10d2e26ea740766969f3a9305be2
Lucas <lucasthormann@protonmail.com>
2025-01-09 14:09:05 -0800

    Strings and casting

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -10,28 +10,48 @@ namespace MyFirstProject
     {
         static void Main(string[] args)
         {
-            int age = 23;
+
+            string textAge = "23";
+            int age = Convert.ToInt32(textAge);
             Console.WriteLine(age);
+            Console.WriteLine("----------------------------------------------------------");
+            Console.WriteLine("Int size: ");
             Console.WriteLine(int.MaxValue);
             Console.WriteLine(int.MinValue);
 
-            long bigNumeber = 900000000L;
+            Console.WriteLine();
+            string textBigNumber = "900000000";
+            long bigNumeber = Convert.ToInt64(textBigNumber);
             Console.WriteLine(bigNumeber);
+            Console.WriteLine("----------------------------------------------------------");
+            Console.WriteLine("Long size: ");
             Console.WriteLine(long.MaxValue);
             Console.WriteLine(long.MinValue);
 
-            double negative = -55.2D;
+            Console.WriteLine();
+            string textNegative = "-55.2";
+            double negative = Convert.ToDouble(textNegative);
             Console.WriteLine(negative);
+            Console.WriteLine("----------------------------------------------------------");
+            Console.WriteLine("Double size: ");
             Console.WriteLine(double.MaxValue);
             Console.WriteLine(double.MinValue);
 
-            float precision = -5.000000001F;
+            Console.WriteLine();
+            string textPrecision = "-5.0000001";
+            float precision = Convert.ToSingle(textPrecision);
             Console.WriteLine(precision);
+            Console.WriteLine("----------------------------------------------------------");
+            Console.WriteLine("Float size: ");
             Console.WriteLine(float.MaxValue);
             Console.WriteLine(float.MinValue);
 
-            decimal money = 214.99M;
+            Console.WriteLine();
+            string textMoney = "214.99";
+            decimal money = Convert.ToDecimal(textMoney);
             Console.WriteLine(money);
+            Console.WriteLine("----------------------------------------------------------");
+            Console.WriteLine("Decimal size: ");
             Console.WriteLine(decimal.MaxValue);
             Console.WriteLine(decimal.MinValue);
 
