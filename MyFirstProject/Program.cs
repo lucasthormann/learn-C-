9eaf38233b1df16b5f23f54036618da9cb385a45
Lucas <lucasthormann@protonmail.com>
2025-01-10 09:14:29 -0800

    TryParse and Loops

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -11,52 +11,52 @@ namespace MyFirstProject
         static void Main(string[] args)
         {
 
-           /* string textAge = "23";
-            int age = Convert.ToInt32(textAge);
-            Console.WriteLine(age);
-            Console.WriteLine("----------------------------------------------------------");
-            Console.WriteLine("Int size: ");
-            Console.WriteLine(int.MaxValue);
-            Console.WriteLine(int.MinValue);
+            /* string textAge = "23";
+             int age = Convert.ToInt32(textAge);
+             Console.WriteLine(age);
+             Console.WriteLine("----------------------------------------------------------");
+             Console.WriteLine("Int size: ");
+             Console.WriteLine(int.MaxValue);
+             Console.WriteLine(int.MinValue);
 
-            Console.WriteLine();
-            string textBigNumber = "900000000";
-            long bigNumeber = Convert.ToInt64(textBigNumber);
-            Console.WriteLine(bigNumeber);
-            Console.WriteLine("----------------------------------------------------------");
-            Console.WriteLine("Long size: ");
-            Console.WriteLine(long.MaxValue);
-            Console.WriteLine(long.MinValue);
+             Console.WriteLine();
+             string textBigNumber = "900000000";
+             long bigNumeber = Convert.ToInt64(textBigNumber);
+             Console.WriteLine(bigNumeber);
+             Console.WriteLine("----------------------------------------------------------");
+             Console.WriteLine("Long size: ");
+             Console.WriteLine(long.MaxValue);
+             Console.WriteLine(long.MinValue);
 
-            Console.WriteLine();
-            string textNegative = "-55.2";
-            double negative = Convert.ToDouble(textNegative);
-            Console.WriteLine(negative);
-            Console.WriteLine("----------------------------------------------------------");
-            Console.WriteLine("Double size: ");
-            Console.WriteLine(double.MaxValue);
-            Console.WriteLine(double.MinValue);
+             Console.WriteLine();
+             string textNegative = "-55.2";
+             double negative = Convert.ToDouble(textNegative);
+             Console.WriteLine(negative);
+             Console.WriteLine("----------------------------------------------------------");
+             Console.WriteLine("Double size: ");
+             Console.WriteLine(double.MaxValue);
+             Console.WriteLine(double.MinValue);
 
-            Console.WriteLine();
-            string textPrecision = "-5.0000001";
-            float precision = Convert.ToSingle(textPrecision);
-            Console.WriteLine(precision);
-            Console.WriteLine("----------------------------------------------------------");
-            Console.WriteLine("Float size: ");
-            Console.WriteLine(float.MaxValue);
-            Console.WriteLine(float.MinValue);
+             Console.WriteLine();
+             string textPrecision = "-5.0000001";
+             float precision = Convert.ToSingle(textPrecision);
+             Console.WriteLine(precision);
+             Console.WriteLine("----------------------------------------------------------");
+             Console.WriteLine("Float size: ");
+             Console.WriteLine(float.MaxValue);
+             Console.WriteLine(float.MinValue);
 
-            Console.WriteLine();
-            string textMoney = "214.99";
-            decimal money = Convert.ToDecimal(textMoney);
-            Console.WriteLine(money);
-            Console.WriteLine("----------------------------------------------------------");
-            Console.WriteLine("Decimal size: ");
-            Console.WriteLine(decimal.MaxValue);
-            Console.WriteLine(decimal.MinValue);*/
+             Console.WriteLine();
+             string textMoney = "214.99";
+             decimal money = Convert.ToDecimal(textMoney);
+             Console.WriteLine(money);
+             Console.WriteLine("----------------------------------------------------------");
+             Console.WriteLine("Decimal size: ");
+             Console.WriteLine(decimal.MaxValue);
+             Console.WriteLine(decimal.MinValue);*/
 
 
-            string textNum = Console.ReadLine();
+            /*string textNum = Console.ReadLine();
             int num = Convert.ToInt32(textNum);
             if(num < 18)
             {
@@ -101,6 +101,27 @@ namespace MyFirstProject
             //conditional operator
             string result = day >= 8 || day <= 0 ? "Invalid" : "Valid";
             Console.WriteLine(result);
+            Console.ReadLine();*/
+
+
+            bool success = true;
+
+            while (success)
+            {
+                Console.WriteLine("Enter a number: ");
+                string numInput = Console.ReadLine();
+
+                if(int.TryParse(numInput, out  int num)) {
+                    success = false;
+                    Console.WriteLine(num);
+
+                }
+                else
+                {
+                    Console.WriteLine("Failed to convert!");
+                }
+
+            }
             Console.ReadLine();
         }
     }
